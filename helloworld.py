ecb4018c6ab9ec0af301f18e946f304014c9ea86
Surinder Kumar <surinder11@gmail.com>
2015-07-25 22:38:36 +0530

    Added new file which returns sum of two numbers.

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+
+class ABC(object):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def sum(self):
+        """Calculate the sum of two numbers"""
+        return (self.a + self.b)
+
+objA = ABC(20, 10)
+print(sum(objA.sum()))
